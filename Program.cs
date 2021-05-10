65da9e686cd55fc3e09f46e3dcaebe0d1779c62a
Kerem Durak <54421885+kerem97@users.noreply.github.com>
2021-05-10 04:20:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp78
+{
+    class Program
+    {
+       
+        private static bool isPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number-1; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+               
+            }
+            return result;
+        }
+         static void Main(string[] args)
+        {
+            if (isPrimeNumber(4))
+            {
+                Console.WriteLine("Prime number");
+            }
+            else
+            {
+                Console.WriteLine("Not prime number");
+            }
+        }
+    }
+ 
+}
